b55413006d035e6da6c270a942ad915a0ca9f259
Hesam Bayat <hesam.bayat@gmail.com>
2021-11-24 13:47:27 +0400

    Update form elements styles

--- src/components/el-checkbox/index.vue
+++ src/components/el-checkbox/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="form-element form-element--checkbox">
+  <div class="checkbox">
     <label>
       <input type="checkbox" :checked="modelValue === 'yes'" :value="modelValue" @change="$emit('update:modelValue', $event.target.checked ? 'yes' : 'no')" />
-      <span class="form-element__label" v-if="!!label">{{ label }}</span>
-      <span class="form-element__error-message" v-if="!!errorMessage">{{ errorMessage }}</span>
+      <span class="checkbox__mock">
+        <svg width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M9.51185 1.66669L3.33334 7.8452L0.488159 5.00002L1.66667 3.82151L3.33334 5.48817L8.33334 0.488174L9.51185 1.66669Z"/>
+        </svg>
+      </span>
+      <span class="checkbox__label" v-if="!!label">{{ label }}</span>
+      <span class="checkbox__error-message" v-if="!!errorMessage">{{ errorMessage }}</span>
     </label>
   </div>
 </template>
@@ -15,3 +20,83 @@ export default {
   emits: ['update:modelValue'],
 }
 </script>
+
+<style lang="scss">
+.checkbox {
+  margin-bottom: $container-padding-y * 2.25;
+
+  label {    
+    display: flex;
+    position: relative;
+    flex-direction: column;
+    padding-left: 60px;
+    padding-bottom: 20px;
+  }
+
+  input {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 20px;
+    height: 20px;
+    opacity: 0;
+  }
+
+  &__mock {
+    display: flex;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 40px;
+    height: 40px;
+    justify-content: center;
+    align-items: center;
+    border: 5px solid;
+    border-radius: 0;
+    background-color: $color-background;
+    color: inherit;
+
+    @include make-decorate();
+
+    svg {
+      fill: currentColor;
+      width: 20px;
+      height: 16px;
+      opacity: 0;
+    }
+
+    @at-root input:checked + #{&} {
+      svg {
+        opacity: 1;
+      }
+    }
+  }
+
+  &__label {
+    display: block;
+    width: 100%;
+    font-size: 18px;
+    font-weight: $font-weight-bold;
+    line-height: 24px;
+    user-select: none;
+  }
+
+  &__error-message {
+    display: block;
+    position: absolute;
+    max-width: 95%;
+    top: 100%;
+    left: 5%;
+    padding: 14px 4% 10px;
+    font-size: 21px;
+    font-weight: $font-weight-bold;
+    line-height: 24px;
+    transform: translateY(-20px) rotate(2deg);
+    pointer-events: none;
+    background-color: $color-orange;
+    color: $color-body;
+
+    @include make-decorate(-4px, 5px);
+  }
+}
+</style>
--- src/components/el-input/index.vue
+++ src/components/el-input/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="form-element form-element--input" :class="{ error: !!errorMessage }">
+  <div class="input" :class="{ error: !!errorMessage }">
     <label>
-      <span class="form-element__label" v-if="!!label">{{ label }}</span>
+      <span class="input__label" v-if="!!label">{{ label }}</span>
       <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" :type="type" :autocomplete="autocomplete" />
-      <span class="form-element__error-message" v-if="!!errorMessage">{{ errorMessage }}</span>
+      <span class="input__error-message" v-if="!!errorMessage">{{ errorMessage }}</span>
     </label>
   </div>
 </template>
@@ -15,3 +15,63 @@ export default {
   emits: ['update:modelValue'],
 }
 </script>
+
+<style lang="scss">
+.input {
+  margin-bottom: $container-padding-y * 1.5;
+
+  label {    
+    display: flex;
+    position: relative;
+    flex-direction: column;
+  }
+
+  input {
+    display: block;
+    width: 100%;
+    max-width: 100%;
+    padding: 10px 14px 0;
+    margin: 0;
+    font-family: $font-family;
+    font-weight: $font-weight-bold;
+    font-size: 48px;
+    line-height: 1.25;
+    border: 5px solid;
+    border-radius: 0;
+    background-color: $color-background;
+    color: inherit;
+    
+    @include make-decorate();
+
+    appearance: none;
+  }
+
+  &__label {
+    display: block;
+    width: 100%;
+    padding-left: 22px;
+    font-size: 40px;
+    font-weight: $font-weight-bold;
+    line-height: 42px;
+    user-select: none;
+  }
+
+  &__error-message {
+    display: block;
+    position: absolute;
+    max-width: 95%;
+    top: 100%;
+    left: 5%;
+    padding: 14px 4% 10px;
+    font-size: 21px;
+    font-weight: $font-weight-bold;
+    line-height: 24px;
+    transform: translateY(-20px) rotate(2deg);
+    pointer-events: none;
+    background-color: $color-orange;
+    color: $color-body;
+
+    @include make-decorate(-4px, 5px);
+  }
+}
+</style>
